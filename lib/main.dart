61d914eceeaa8f9f7a1d73c63f45c8319c0217b4
Phuthita Sookhong <phuthita24426@gmail.com>
2021-10-09 21:51:37 +0700

    ้finallllllll

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
         HomePage.routeName: (context) => const HomePage(),
         FoodDetail.routeName: (context) =>const FoodDetail(),
       },
-      initialRoute: HomePage.routeName,
+      initialRoute: LoginPage.routeName,
     );
   }
 }
